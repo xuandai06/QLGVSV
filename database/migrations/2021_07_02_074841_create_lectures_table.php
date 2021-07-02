7dc6a2b94ea5c07ad3e7f103f855c664f2fe1f70
xuandai06 <84690681+xuandai06@users.noreply.github.com>
2021-07-02 16:26:23 +0700

    update lecturer database 2/7

---
+++ database/migrations/2021_07_02_074841_create_lectures_table.php
@@ -0,0 +1,51 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateLecturesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('lectures', function (Blueprint $table) {
+            $table->string('id')->primary();
+            $table->string('name',100);
+            $table->string('sex',10);
+            $table->date('birthday');
+            $table->string('hometown');
+            $table->string('address');
+            $table->string('phone_number',13);
+            $table->string('note');
+            $table->string('position_id',15);
+            $table->string('level_id',15);
+            $table->string('major_id',15);
+
+            $table->foreign('position_id')->references('id')->on('positions')
+            ->onUpdate('cascade')
+            ->onDelete('cascade');
+            $table->foreign('level_id')->references('id')->on('levels')
+            ->onUpdate('cascade')
+            ->onDelete('cascade');
+            $table->foreign('major_id')->references('id')->on('majors')
+            ->onUpdate('cascade')
+            ->onDelete('cascade');
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('lectures');
+    }
+}
