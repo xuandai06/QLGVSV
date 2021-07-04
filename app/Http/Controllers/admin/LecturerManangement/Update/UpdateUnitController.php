1ea28f723c59e54ca732829b5a65f877dc0bcae0
xuandai06 <84690681+xuandai06@users.noreply.github.com>
2021-07-04 12:08:57 +0700

    update controller

---
+++ app/Http/Controllers/admin/LecturerManangement/Update/UpdateUnitController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers\Admin\LecturerManangement\Update;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class UpdateUnitController extends Controller
+{
+    //
+}
--- storage/framework/views/924ceeb6ab9c53209079d922a2643ec0fdf8f662.php
+++ storage/framework/views/924ceeb6ab9c53209079d922a2643ec0fdf8f662.php
@@ -6,14 +6,14 @@
             <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6" />
             </svg>
-            <a href="<?php echo e(route('home')); ?>" class="clear-left p-2">Home</a>
+            <a href="<?php echo e(route('home')); ?>" class="clear-left p-2">Trang chủ</a>
         </div>
 
         <div class="flex items-center rounded-lg duration-300 ease-in-out hover:bg-blue-200 px-2 hover:text-white p-1">
             <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M10 6H5a2 2 0 00-2 2v9a2 2 0 002 2h14a2 2 0 002-2V8a2 2 0 00-2-2h-5m-4 0V5a2 2 0 114 0v1m-4 0a2 2 0 104 0m-5 8a2 2 0 100-4 2 2 0 000 4zm0 0c1.306 0 2.417.835 2.83 2M9 14a3.001 3.001 0 00-2.83 2M15 11h3m-3 4h2" />
             </svg>
-            <button class="clear-left p-2" id="teacher">dai ngu</button>
+            <button class="clear-left p-2" id="teacher">Thông tin</button>
         </div>
 
         <div class="w-full bg-white hidden flex-col justify-between px-6 text-gray-500 rounded-lg mt-1" id="dropteacher">
@@ -43,7 +43,7 @@
             <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M10 6H5a2 2 0 00-2 2v9a2 2 0 002 2h14a2 2 0 002-2V8a2 2 0 00-2-2h-5m-4 0V5a2 2 0 114 0v1m-4 0a2 2 0 104 0m-5 8a2 2 0 100-4 2 2 0 000 4zm0 0c1.306 0 2.417.835 2.83 2M9 14a3.001 3.001 0 00-2.83 2M15 11h3m-3 4h2" />
             </svg>
-            <button class="clear-left p-2" id="student">Hung ngu</button>
+            <button class="clear-left p-2" id="student">NCKH-SV</button>
         </div>
 
         <div class="w-full bg-white hidden flex-col justify-between px-6 text-gray-500 rounded-lg mt-1" id="dropstudent">
