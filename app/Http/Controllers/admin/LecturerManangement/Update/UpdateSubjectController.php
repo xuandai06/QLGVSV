ec2b507ae6628eab25ad80ea3b7888695d490f8b
xuandai06 <84690681+xuandai06@users.noreply.github.com>
2021-07-04 12:32:56 +0700

    update xoa may  cai linh tinh

--- app/Http/Controllers/admin/LecturerManangement/Update/UpdateSubjectController.php
+++ app/Http/Controllers/admin/LecturerManangement/Update/UpdateSubjectController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers\Admin\LecturerManangement\Update;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
-class UpdateUnitController extends Controller
+class UpdateSubjectController extends Controller
 {
-    
+    //
 }
--- routes/web.php
+++ routes/web.php
@@ -20,9 +20,6 @@ Route::get('/logout', [LogoutController::class, 'logout'])->name('logout');
 Route::get('/admin', [AdminController::class, 'index'])->name('admin')
 ->middleware('AdminMiddleware');
 
-Route::get('/update_gv', [AdminController::class, 'index'])->name('admin')
-->middleware('AdminMiddleware');
-
 Route::get('/update_gv', function () {
     return view('layouts.admin.lecturer_management.update.update');
 })->name('update_gv')->middleware('AdminMiddleware');
@@ -32,14 +29,4 @@ Route::get('/lecturer', [LecturerController::class, 'index'])->name('lecturer')-
 Route::get('/student', [StudentController::class, 'index'])->name('student');
 
 //trang cap nhat don vi cua phan admin
-
-//trang chu cua admin
-Route::get('/home_admin', function () {
-    return view('layouts.admin.home');
-})->name('home_admin');
-
-//trang cap nhat don vi cua phan amin
-Route::get('/update_gv', function () {
-    return view('layouts.admin.lecturer_management.update.update');
-})->name('update_gv');
 
