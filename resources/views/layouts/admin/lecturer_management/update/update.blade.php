553c6884fee8aff0093ed177d693fb6561951bb4
HoangHiep2001HVU <84690670+HoangHiep2001HVU@users.noreply.github.com>
2021-07-04 12:48:05 +0700

    them update

--- resources/views/layouts/admin/lecturer_management/update/update.blade.php
+++ resources/views/layouts/admin/lecturer_management/update/update.blade.php
@@ -19,10 +19,10 @@
 
                             <div class="mb-4 flex">
                                 <p class="text-gray-500 text-xl w-2/12 pt-3">Mã đơn vị: </p>
-                                <input type="text" name="id_unit" id="id_unit" placeholder="Nhập vào mã đơn vị..." class="bg-white w-full p-4 rounded-lg
-                                border-2 focus:outline-none focus:ring-2 focus:ring-blue-400 focus:border-transparent @error('id_unit') border-red-500 @enderror" value="">
+                                <input type="text" name="id" id="id" placeholder="Nhập vào mã đơn vị..." class="bg-white w-full p-4 rounded-lg
+                                border-2 focus:outline-none focus:ring-2 focus:ring-blue-400 focus:border-transparent @error('id') border-red-500 @enderror" value="">
 
-                                @error('id_unit')
+                                @error('id')
                                 <div class="text-red-500 mt-2 text-sm">
                                     {{ $message }}
                                 </div>
@@ -30,11 +30,11 @@
                             </div>
 
                             <div class="mb-4 flex">
-                            <p class="text-gray-500 text-xl w-2/12 pt-3">Tên đơn vị: </p>
-                                <input type="text" name="name_unit" id="name_unit" placeholder="Nhập vào tên đơn vị ..." class="bg-white w-full p-4 rounded-lg
-                                border-2 focus:outline-none focus:ring-2 focus:ring-blue-400 focus:border-transparent @error('password') border-red-500 @enderror" value="">
+                                <p class="text-gray-500 text-xl w-2/12 pt-3">Tên đơn vị: </p>
+                                <input type="text" name="name" id="name" placeholder="Nhập vào tên đơn vị ..." class="bg-white w-full p-4 rounded-lg
+                                border-2 focus:outline-none focus:ring-2 focus:ring-blue-400 focus:border-transparent @error('name') border-red-500 @enderror" value="">
 
-                                @error('name_unit')
+                                @error('name')
                                 <div class="text-red-500 mt-2 text-sm">
                                     {{ $message }}
                                 </div>
@@ -47,21 +47,120 @@
                         </form>
                     </div>
                 </div>
-
             </div>
 
             <div class="flex items-center rounded-lg duration-300 ease-in-out hover:bg-blue-200 px-2 hover:text-white p-1">
                 <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M7 16a4 4 0 01-.88-7.903A5 5 0 1115.9 6L16 6a5 5 0 011 9.9M15 13l-3-3m0 0l-3 3m3-3v12" />
                 </svg>
-                <button class="clear-left p-2">Cập nhật thông tin về bộ môn</button>
+                <button class="clear-left p-2" id="subjects">Cập nhật thông tin về bộ môn</button>
+            </div>
+
+            <div class="w-full bg-gray-200 hidden flex-col justify-between px-6 text-gray-500 rounded-lg mt-1" id="dropsubjects">
+                <div class="w-full p-2 rounded-lg flex justify-center">
+                    <div class="p-2 w-10/12 p-6">
+                        <form action="" method="" class="w-10/12 flex-col justify-center">
+                            @csrf
+
+                            <div class="mb-4 flex">
+                                <p class="text-gray-500 text-xl w-2/12 pt-3">Mã bộ môn: </p>
+                                <input type="text" name="id" id="id" placeholder="Nhập vào mã bộ môn..." class="bg-white w-full p-4 rounded-lg
+                                border-2 focus:outline-none focus:ring-2 focus:ring-blue-400 focus:border-transparent @error('id') border-red-500 @enderror" value="">
+
+                                @error('id')
+                                <div class="text-red-500 mt-2 text-sm">
+                                    {{ $message }}
+                                </div>
+                                @enderror
+                            </div>
+
+                            <div class="mb-4 flex">
+                                <p class="text-gray-500 text-xl w-2/12 pt-3">Tên bộ môn: </p>
+                                <input type="text" name="name" id="name" placeholder="Nhập vào tên bộ môn ..." class="bg-white w-full p-4 rounded-lg
+                                border-2 focus:outline-none focus:ring-2 focus:ring-blue-400 focus:border-transparent @error('name') border-red-500 @enderror" value="">
+
+                                @error('name')
+                                <div class="text-red-500 mt-2 text-sm">
+                                    {{ $message }}
+                                </div>
+                                @enderror
+                            </div>
+
+                            <div class="mb-4 flex">
+                                <p class="text-gray-500 text-xl w-2/12 pt-3">Mã đơn vị: </p>
+                                <input type="text" name="id" id="id" placeholder="Nhập vào mã đơn vị..." class="bg-white w-full p-4 rounded-lg
+                                border-2 focus:outline-none focus:ring-2 focus:ring-blue-400 focus:border-transparent @error('id') border-red-500 @enderror" value="">
+
+                                @error('id')
+                                <div class="text-red-500 mt-2 text-sm">
+                                    {{ $message }}
+                                </div>
+                                @enderror
+                            </div>
+
+                            <div class="flex justify-center">
+                                <button type="submit" class="bg-blue-500 text-white px-4 py-3 rounded font-medium w-3/12">Cập nhập</button>
+                            </div>
+                        </form>
+                    </div>
+                </div>
             </div>
 
             <div class="flex items-center rounded-lg duration-300 ease-in-out hover:bg-blue-200 px-2 hover:text-white p-1">
                 <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M7 16a4 4 0 01-.88-7.903A5 5 0 1115.9 6L16 6a5 5 0 011 9.9M15 13l-3-3m0 0l-3 3m3-3v12" />
                 </svg>
-                <button class="clear-left p-2">Cập nhật thông tin về ngành</button>
+                <button class="clear-left p-2" id="majors">Cập nhật thông tin về ngành</button>
+            </div>
+
+            <div class="w-full bg-gray-200 hidden flex-col justify-between px-6 text-gray-500 rounded-lg mt-1" id="dropmajors">
+                <div class="w-full p-2 rounded-lg flex justify-center">
+                    <div class="p-2 w-10/12 p-6">
+                        <form action="" method="" class="w-10/12 flex-col justify-center">
+                            @csrf
+
+                            <div class="mb-4 flex">
+                                <p class="text-gray-500 text-xl w-2/12 pt-3">Mã ngành: </p>
+                                <input type="text" name="id" id="id" placeholder="Nhập vào mã ngành..." class="bg-white w-full p-4 rounded-lg
+                                border-2 focus:outline-none focus:ring-2 focus:ring-blue-400 focus:border-transparent @error('id') border-red-500 @enderror" value="">
+
+                                @error('id')
+                                <div class="text-red-500 mt-2 text-sm">
+                                    {{ $message }}
+                                </div>
+                                @enderror
+                            </div>
+
+                            <div class="mb-4 flex">
+                                <p class="text-gray-500 text-xl w-2/12 pt-3">Tên ngành: </p>
+                                <input type="text" name="name" id="name" placeholder="Nhập vào tên ngành ..." class="bg-white w-full p-4 rounded-lg
+                                border-2 focus:outline-none focus:ring-2 focus:ring-blue-400 focus:border-transparent @error('name') border-red-500 @enderror" value="">
+
+                                @error('name')
+                                <div class="text-red-500 mt-2 text-sm">
+                                    {{ $message }}
+                                </div>
+                                @enderror
+                            </div>
+
+                            <div class="mb-4 flex">
+                                <p class="text-gray-500 text-xl w-2/12 pt-3">Mã bộ môn: </p>
+                                <input type="text" name="id" id="id" placeholder="Nhập vào mã bộ môn..." class="bg-white w-full p-4 rounded-lg
+                                border-2 focus:outline-none focus:ring-2 focus:ring-blue-400 focus:border-transparent @error('id') border-red-500 @enderror" value="">
+
+                                @error('id')
+                                <div class="text-red-500 mt-2 text-sm">
+                                    {{ $message }}
+                                </div>
+                                @enderror
+                            </div>
+
+                            <div class="flex justify-center">
+                                <button type="submit" class="bg-blue-500 text-white px-4 py-3 rounded font-medium w-3/12">Cập nhập</button>
+                            </div>
+                        </form>
+                    </div>
+                </div>
             </div>
 
             <div class="flex items-center rounded-lg duration-300 ease-in-out hover:bg-blue-200 px-2 hover:text-white p-1">
@@ -91,11 +190,25 @@
         window.addEventListener('DOMContentLoaded', () => {
             const unit = document.querySelector('#unit')
             const dropunit = document.querySelector('#dropunit')
-
+            const subjects = document.querySelector('#subjects')
+            const dropsubjects = document.querySelector('#dropsubjects')
 
             unit.addEventListener('click', () => {
                 dropunit.classList.toggle('hidden')
                 dropunit.classList.toggle('flex')
+
+                if (dropsubjects.classList.contains('hidden') == false) {
+                    dropsubjects.classList.toggle('hidden')
+                };
+            })
+
+            subjects.addEventListener('click', () => {
+                dropsubjects.classList.toggle('hidden')
+                dropsubjects.classList.toggle('flex')
+
+                if (dropunit.classList.contains('hidden') == false) {
+                    dropunit.classList.add('hidden');
+                };
             })
 
         })
--- storage/framework/views/c0a1e23ba250e945c6254c803a78ea25b161962a.php
+++ storage/framework/views/c0a1e23ba250e945c6254c803a78ea25b161962a.php
@@ -45,7 +45,7 @@ unset($__errorArgs, $__bag); ?>
                             </div>
 
                             <div class="mb-4 flex">
-                            <p class="text-gray-500 text-xl w-2/12 pt-3">Tên đơn vị: </p>
+                                <p class="text-gray-500 text-xl w-2/12 pt-3">Tên đơn vị: </p>
                                 <input type="text" name="name_unit" id="name_unit" placeholder="Nhập vào tên đơn vị ..." class="bg-white w-full p-4 rounded-lg
                                 border-2 focus:outline-none focus:ring-2 focus:ring-blue-400 focus:border-transparent <?php $__errorArgs = ['password'];
 $__bag = $errors->getBag($__errorArgs[1] ?? 'default');
@@ -77,14 +77,108 @@ unset($__errorArgs, $__bag); ?>
                         </form>
                     </div>
                 </div>
-
             </div>
 
             <div class="flex items-center rounded-lg duration-300 ease-in-out hover:bg-blue-200 px-2 hover:text-white p-1">
                 <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M7 16a4 4 0 01-.88-7.903A5 5 0 1115.9 6L16 6a5 5 0 011 9.9M15 13l-3-3m0 0l-3 3m3-3v12" />
                 </svg>
-                <button class="clear-left p-2">Cập nhật thông tin về bộ môn</button>
+                <button class="clear-left p-2" id="subjects">Cập nhật thông tin về bộ môn</button>
+            </div>
+
+            <div class="w-full bg-gray-200 hidden flex-col justify-between px-6 text-gray-500 rounded-lg mt-1" id="dropsubjects">
+                <div class="w-full p-2 rounded-lg flex justify-center">
+                    <div class="p-2 w-10/12 p-6">
+                        <form action="" method="" class="w-10/12 flex-col justify-center">
+                            <?php echo csrf_field(); ?>
+
+                            <div class="mb-4 flex">
+                                <p class="text-gray-500 text-xl w-2/12 pt-3">Mã bộ môn: </p>
+                                <input type="text" name="id_subjects" id="id_subjects" placeholder="Nhập vào mã bộ môn..." class="bg-white w-full p-4 rounded-lg
+                                border-2 focus:outline-none focus:ring-2 focus:ring-blue-400 focus:border-transparent <?php $__errorArgs = ['id_unit'];
+$__bag = $errors->getBag($__errorArgs[1] ?? 'default');
+if ($__bag->has($__errorArgs[0])) :
+if (isset($message)) { $__messageOriginal = $message; }
+$message = $__bag->first($__errorArgs[0]); ?> border-red-500 <?php unset($message);
+if (isset($__messageOriginal)) { $message = $__messageOriginal; }
+endif;
+unset($__errorArgs, $__bag); ?>" value="">
+
+                                <?php $__errorArgs = ['id_subjects'];
+$__bag = $errors->getBag($__errorArgs[1] ?? 'default');
+if ($__bag->has($__errorArgs[0])) :
+if (isset($message)) { $__messageOriginal = $message; }
+$message = $__bag->first($__errorArgs[0]); ?>
+                                <div class="text-red-500 mt-2 text-sm">
+                                    <?php echo e($message); ?>
+
+                                </div>
+                                <?php unset($message);
+if (isset($__messageOriginal)) { $message = $__messageOriginal; }
+endif;
+unset($__errorArgs, $__bag); ?>
+                            </div>
+
+                            <div class="mb-4 flex">
+                                <p class="text-gray-500 text-xl w-2/12 pt-3">Tên bộ môn: </p>
+                                <input type="text" name="name_subjects" id="name_subjects" placeholder="Nhập vào tên bộ môn ..." class="bg-white w-full p-4 rounded-lg
+                                border-2 focus:outline-none focus:ring-2 focus:ring-blue-400 focus:border-transparent <?php $__errorArgs = ['password'];
+$__bag = $errors->getBag($__errorArgs[1] ?? 'default');
+if ($__bag->has($__errorArgs[0])) :
+if (isset($message)) { $__messageOriginal = $message; }
+$message = $__bag->first($__errorArgs[0]); ?> border-red-500 <?php unset($message);
+if (isset($__messageOriginal)) { $message = $__messageOriginal; }
+endif;
+unset($__errorArgs, $__bag); ?>" value="">
+
+                                <?php $__errorArgs = ['name_subjects'];
+$__bag = $errors->getBag($__errorArgs[1] ?? 'default');
+if ($__bag->has($__errorArgs[0])) :
+if (isset($message)) { $__messageOriginal = $message; }
+$message = $__bag->first($__errorArgs[0]); ?>
+                                <div class="text-red-500 mt-2 text-sm">
+                                    <?php echo e($message); ?>
+
+                                </div>
+                                <?php unset($message);
+if (isset($__messageOriginal)) { $message = $__messageOriginal; }
+endif;
+unset($__errorArgs, $__bag); ?>
+                            </div>
+
+                            <div class="mb-4 flex">
+                                <p class="text-gray-500 text-xl w-2/12 pt-3">Mã đơn vị: </p>
+                                <input type="text" name="id_unit" id="id_unit" placeholder="Nhập vào mã đơn vị..." class="bg-white w-full p-4 rounded-lg
+                                border-2 focus:outline-none focus:ring-2 focus:ring-blue-400 focus:border-transparent <?php $__errorArgs = ['id_unit'];
+$__bag = $errors->getBag($__errorArgs[1] ?? 'default');
+if ($__bag->has($__errorArgs[0])) :
+if (isset($message)) { $__messageOriginal = $message; }
+$message = $__bag->first($__errorArgs[0]); ?> border-red-500 <?php unset($message);
+if (isset($__messageOriginal)) { $message = $__messageOriginal; }
+endif;
+unset($__errorArgs, $__bag); ?>" value="">
+
+                                <?php $__errorArgs = ['id_unit'];
+$__bag = $errors->getBag($__errorArgs[1] ?? 'default');
+if ($__bag->has($__errorArgs[0])) :
+if (isset($message)) { $__messageOriginal = $message; }
+$message = $__bag->first($__errorArgs[0]); ?>
+                                <div class="text-red-500 mt-2 text-sm">
+                                    <?php echo e($message); ?>
+
+                                </div>
+                                <?php unset($message);
+if (isset($__messageOriginal)) { $message = $__messageOriginal; }
+endif;
+unset($__errorArgs, $__bag); ?>
+                            </div>
+
+                            <div class="flex justify-center">
+                                <button type="submit" class="bg-blue-500 text-white px-4 py-3 rounded font-medium w-3/12">Cập nhập</button>
+                            </div>
+                        </form>
+                    </div>
+                </div>
             </div>
 
             <div class="flex items-center rounded-lg duration-300 ease-in-out hover:bg-blue-200 px-2 hover:text-white p-1">
@@ -121,11 +215,25 @@ unset($__errorArgs, $__bag); ?>
         window.addEventListener('DOMContentLoaded', () => {
             const unit = document.querySelector('#unit')
             const dropunit = document.querySelector('#dropunit')
-
+            const subjects = document.querySelector('#subjects')
+            const dropsubjects = document.querySelector('#dropsubjects')
 
             unit.addEventListener('click', () => {
                 dropunit.classList.toggle('hidden')
                 dropunit.classList.toggle('flex')
+
+                if (dropsubjects.classList.contains('hidden') == false) {
+                    dropsubjects.classList.toggle('hidden')
+                };
+            })
+
+            subjects.addEventListener('click', () => {
+                dropsubjects.classList.toggle('hidden')
+                dropsubjects.classList.toggle('flex')
+
+                if (dropunit.classList.contains('hidden') == false) {
+                    dropunit.classList.add('hidden');
+                };
             })
 
         })
