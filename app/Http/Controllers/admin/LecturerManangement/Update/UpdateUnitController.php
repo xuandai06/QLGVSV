d02f8037c43adc53e617b3a42bc3e1deab999fe5
xuandai06 <84690681+xuandai06@users.noreply.github.com>
2021-07-04 16:40:05 +0700

    up

--- app/Http/Controllers/admin/LecturerManangement/Update/UpdateUnitController.php
+++ app/Http/Controllers/admin/LecturerManangement/Update/UpdateUnitController.php
@@ -3,11 +3,16 @@
 namespace App\Http\Controllers\Admin\LecturerManangement\Update;
 
 use App\Http\Controllers\Controller;
+use App\Models\Unit;
 use Illuminate\Http\Request;
 
 class UpdateUnitController extends Controller
 {
     public function store(Request $request){
-        return $request;
+        $unit = new Unit;
+        $unit->id = $request->id;
+        $unit->name = $request->name;
+        $unit->save();
+        return back();
     }
 }
