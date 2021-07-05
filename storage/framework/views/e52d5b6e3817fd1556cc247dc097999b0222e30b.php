e808a64f89660868733a8c1791a6bdc551a4c6e6
HoangHiep2001HVU <84690670+HoangHiep2001HVU@users.noreply.github.com>
2021-07-05 15:09:42 +0700

    sua

--- storage/framework/views/e52d5b6e3817fd1556cc247dc097999b0222e30b.php
+++ storage/framework/views/e52d5b6e3817fd1556cc247dc097999b0222e30b.php
@@ -1,7 +1,7 @@
 
 
 <?php $__env->startSection('content'); ?>
-    <div class="flex justify-center">
+    <div class="flex justify-center mt-2">
         <div class="w-8/12 bg-white p-6 border-double border-4 border-gray-400 rounded-lg">
             home
         </div>
