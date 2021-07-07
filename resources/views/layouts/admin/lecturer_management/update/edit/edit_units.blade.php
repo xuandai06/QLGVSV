ece5e410641a716b73dfdefefe83c0b16952f04d
xuandai06 <84690681+xuandai06@users.noreply.github.com>
2021-07-07 20:44:56 +0700

    commit lai

--- resources/views/layouts/admin/lecturer_management/update/edit/edit_units.blade.php
+++ resources/views/layouts/admin/lecturer_management/update/edit/edit_units.blade.php
@@ -1,27 +1,28 @@
-<<<<<<< HEAD
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
+
 <body>
-            <form action="{{route('edit/units', $unit)}}" method="post">
-                 @csrf
-                <input type="text" id="temp_id" name="temp_id" 
-                value="{{$unit->id }}" disabled>
-                <input type="text" id="name" name="name" value="{{old('name') ?? $unit->name}}">
-                @error('name')
-                    {{$message}}
-                @enderror
-                <button type="submit">save</button>
-            </form>
-           
+    <form action="{{route('edit/units', $unit)}}" method="post">
+        @csrf
+        <input type="text" id="temp_id" name="temp_id" value="{{$unit->id }}" disabled>
+        <input type="text" id="name" name="name" value="{{old('name') ?? $unit->name}}">
+        @error('name')
+        {{$message}}
+        @enderror
+        <button type="submit">save</button>
+    </form>
+
 </body>
+
 </html>
-=======
+
 @extends('layouts.admin.lecturer_management.menu_update')
 
 @section('update_lecturer')
@@ -116,4 +117,3 @@
 </div>
 
 @endsection
->>>>>>> 579d791cafa06505fe2ead811660eabf84c81fac
