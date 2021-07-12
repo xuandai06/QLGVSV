ddab5143329d0f06eb97f47b22a4f7fede762a48
hungtm0912 <84690430+hungtm0912@users.noreply.github.com>
2021-07-12 08:26:28 +0700

    update quan ly NCKH-GV

--- storage/framework/views/c5ba144a91dec9be900d47ba1a4b8f53b30308e6.php
+++ storage/framework/views/c5ba144a91dec9be900d47ba1a4b8f53b30308e6.php
@@ -14,26 +14,26 @@
                         </li>
 
                         <li class="px-2 py-3 rounded-lg hover:bg-blue-400 hover:text-white">
-                            <a href="<?php echo e(route('update/subjects')); ?>">=> Cập nhật danh mục tạp chí</a>
+                            <a href="<?php echo e(route('/nckh_update/magazine')); ?>">=> Cập nhật danh mục tạp chí</a>
                         </li>
 
                         <li class="px-2 py-3 rounded-lg hover:bg-blue-400 hover:text-white">
-                            <a href="<?php echo e(route('update/majors')); ?>">=> Cập nhật thông tin chi tiết về đề tài  NCKH hay giáo trình/ bài báo</a>
+                            <a href="<?php echo e(route('/nckh_update/info')); ?>">=> Cập nhật thông tin chi tiết về đề tài  NCKH hay giáo trình/ bài báo</a>
                         </li>
 
                         <li class="px-2 py-3 rounded-lg hover:bg-blue-400 hover:text-white">
-                            <a href="<?php echo e(route('update/positions')); ?>">=> Cập nhật thông tin về vai trò tham gia của GV vào NCKH hay giáo trình/ bài báo</a>
+                            <a href="<?php echo e(route('/nckh_update/role')); ?>">=> Cập nhật thông tin về vai trò tham gia của GV vào NCKH hay giáo trình/ bài báo</a>
                         </li>
 
                         <li class="px-2 py-3 rounded-lg hover:bg-blue-400 hover:text-white">
-                            <a href="<?php echo e(route('update/levels')); ?>">=> Cập nhật danh mục hội thảo, hội nghị</a>
+                            <a href="<?php echo e(route('/nckh_update/category')); ?>">=> Cập nhật danh mục hội thảo, hội nghị</a>
                         </li>
 
                         <li class="px-2 py-3 rounded-lg hover:bg-blue-400 hover:text-white">
-                            <a href="<?php echo e(route('update/lecturers')); ?>">=> Cập nhật thông tin chi tiết vai trò tham gia của GV vào HN-HT</a>
+                            <a href="<?php echo e(route('/nckh_update/seminor_role')); ?>">=> Cập nhật thông tin chi tiết vai trò tham gia của GV vào HN-HT</a>
                         </li>
                         <li class="px-2 py-3 rounded-lg hover:bg-blue-400 hover:text-white">
-                            <a href="<?php echo e(route('update/lecturers')); ?>">=> Cập nhật về bồi dưỡng của GV</a>
+                            <a href="<?php echo e(route('/nckh_update/fostering')); ?>">=> Cập nhật về bồi dưỡng của GV</a>
                         </li>
                     </ul>
                 </div>
