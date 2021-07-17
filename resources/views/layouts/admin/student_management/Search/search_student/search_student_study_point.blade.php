971c512ad9f3f213454c3237e7a1931fe3e42ccd
hungtm0912 <84690430+hungtm0912@users.noreply.github.com>
2021-07-17 12:03:20 +0700

    day len

---
+++ resources/views/layouts/admin/student_management/Search/search_student/search_student_study_point.blade.php
@@ -0,0 +1,57 @@
+@extends('layouts.menu')
+
+@section('content')
+<div class="flex justify-center mt-2">
+    <div class="w-full bg-white p-6 border-2 border-gray-400 rounded-lg">
+        <div class="flex justify-center text-2xl text-blue-400">
+            <h1>Tìm kiếm theo điểm học tập, rèn luyện</h1>
+        </div>
+        <div class="flex justify-center m-4">
+            <form action="{{route('search/details/lecturers')}}" class="w-6/12 flex justify-between">
+
+                <div class="flex-col">
+                    <p class="text-gray-700">Nhập điểm học tập:</p>
+                    <input type="text" id="keyword" name="keyword" class="p-1 border-2 border-gray-200
+                    focus:outline-none focus:ring-2 focus:ring-blue-400 focus:border-transparent" placeholder="Nhập ...">
+                </div>
+
+                <div class="flex-col">
+                    <p class="text-gray-700">Nhập điểm rèn luyện:</p>
+                    <input type="text" id="keyword" name="keyword" class="p-1 border-2 border-gray-200
+                    focus:outline-none focus:ring-2 focus:ring-blue-400 focus:border-transparent" placeholder="Nhập ...">
+                </div>
+
+                <button type="submit" class="bg-blue-500 text-white rounded px-4 font-medium">Search</button>
+            </form>
+        </div>
+
+        <div class="w-full flex-col border-t-2 border-gray-400">
+            <div class="text-gray-700 text-2xl font-black m-4 mb-6 flex justify-center">
+                <h1 class="">Kết quả tìm kiếm</h1>
+            </div>
+            <table class="bg-white table-fixed flex-col justify-center">
+                <thead>
+                    <tr>
+                        <th class="w-1/12 border-collapse border border-gray-500 p-2">Mã SV</th>
+                        <th class="w-2/12 border-collapse border border-gray-500 p-2">Tên sinh viên</th>
+                        <th class="w-1/12 border-collapse border border-gray-500 p-2">Giới tính</th>
+                        <th class="w-1/12 border-collapse border border-gray-500 p-2">Lớp học</th>
+                        <th class="w-1/12 border-collapse border border-gray-500 p-2">Nội trú</th>
+                        <th class="w-1/12 border-collapse border border-gray-500 p-2">Ngoại trú</th>
+                        <th class="w-1/12 border-collapse border border-gray-500 p-2">Đối tượng chính sách</th>
+                        <th class="w-1/12 border-collapse border border-gray-500 p-2">Khóa học</th>
+                        <th class="w-1/12 border-collapse border border-gray-500 p-2">Năm tốt nghiệp</th>
+                        <th class="w-1/12 border-collapse border border-gray-500 p-2">Thôi học</th>
+                        <th class="w-1/12 border-collapse border border-gray-500 p-2">Khen thưởng</th>
+                        <th class="w-1/12 border-collapse border border-gray-500 p-2">Kỷ luật</th>
+                        <th class="w-1/12 border-collapse border border-gray-500 p-2">Điểm học tập</th>
+                        <th class="w-1/12 border-collapse border border-gray-500 p-2">Điểm rèn luyện</th>
+                        <th class="w-1/12 border-collapse border border-gray-500 p-2">Ghi chú</th>
+                    </tr>
+                </thead>
+
+            </table>
+        </div>
+    </div>
+</div>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -370,6 +370,42 @@ Route::get('/update/student_study_point', function () {
     return view('layouts.admin.student_management.update.update_student_study_point ');
 });
 
+//phan search
+Route::get('/search/student_by_name', function () {
+    return view('layouts.admin.student_management.search.search_student_name ');
+});
+Route::get('/search/student_by_address', function () {
+    return view('layouts.admin.student_management.search.search_student_address ');
+});
+Route::get('/search/student_by_sex', function () {
+    return view('layouts.admin.student_management.search.search_student_sex ');
+});
+Route::get('/search/student_by_course', function () {
+    return view('layouts.admin.student_management.search.search_student_course ');
+});
+Route::get('/search/student_by_boarding_outpatient', function () {
+    return view('layouts.admin.student_management.search.search_student_boarding_outpatient ');
+});
+Route::get('/search/student_by_class', function () {
+    return view('layouts.admin.student_management.search.search_student_class ');
+});
+Route::get('/search/student_by_policy', function () {
+    return view('layouts.admin.student_management.search.search_student_policy ');
+});
+Route::get('/search/student_by_graduate', function () {
+    return view('layouts.admin.student_management.search.search_student_graduate ');
+});
+Route::get('/search/student_by_quit', function () {
+    return view('layouts.admin.student_management.search.search_student_quit ');
+});
+Route::get('/search/student_by_reward_discipline', function () {
+    return view('layouts.admin.student_management.search.search_student_reward_discipline ');
+});
+Route::get('/search/student_by_study_point', function () {
+    return view('layouts.admin.student_management.search.search_student_study_point ');
+});
+
+
 //Het Route cua Hung
 
 
